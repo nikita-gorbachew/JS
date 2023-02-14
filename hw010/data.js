6122d699fd89aa72d0e1fd1486fb4883722d587c
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2023-02-14 22:27:55 +0300

    hw10

---
+++ hw010/data.js
@@ -0,0 +1,79 @@
+const dataInfo = `[
+    {
+      "number": "2118",
+      "language": "en",
+      "insult": "You gamble with God's Word like a rogue.",
+      "created": "2023-02-14 12:19:18",
+      "shown": "3284",
+      "createdby": "Martin Luther",
+      "active": "1",
+      "comment": "https://github.com/stechstudio/insult-service",
+      "image": "https://random.imagecdn.app/510/150"
+    },
+
+    {
+      "number": "2489",
+      "language": "en",
+      "insult": "You make me wish I had more middle fingers.",
+      "created": "2023-02-14 17:54:18",
+      "shown": "3460",
+      "createdby": "Reddit",
+      "active": "1",
+      "comment": "",
+      "image": "https://random.imagecdn.app/500/100"
+    },
+    {
+      "number": "2405",
+      "language": "en",
+      "insult": "Does your asshole get jealous of the shit that comes out of your mouth?",
+      "shown": "3444",
+      "createdby": "9GAG",
+      "active": "1",
+      "comment": "",
+      "image": "https://random.imagecdn.app/400/110"
+    },
+    {
+        "number": "2636",
+        "language": "en",
+        "insult": "I'm going to fuck your dad and give him a child he'll actually love.",
+        "created": "2023-02-14 18:17:37",
+        "shown": "3305",
+        "createdby": "Girl on Twitch",
+        "active": "1",
+        "comment": "Cool",
+        "image": "https://random.imagecdn.app/300/110"
+      },
+      {
+        "number": "2261",
+        "language": "en",
+        "insult": "Thou whoreson zed , thou unnecessary letter!",
+        "created": "2023-02-14 13:37:36",
+        "shown": "3464",
+        "createdby": "No Sweat Shakespeare",
+        "active": "1",
+        "comment": "King Lear (Act 2, Scene 2 )",
+        "image": "https://random.imagecdn.app/420/200"
+      },
+      {
+        "number": "2267",
+        "language": "en",
+        "insult": "You are as a candle, the better burnt out.",
+        "created": "2023-02-14 21:15:08",
+        "shown": "3383",
+        "createdby": "No Sweat Shakespeare",
+        "active": "1",
+        "comment": "Henry IV Part 2 (Act 1, Scene 2)",
+        "image": "https://random.imagecdn.app/405/110"
+      },
+      {
+        "number": "2195",
+        "language": "en",
+        "insult": "Your astute minds have been completely turned into stinking mushrooms.",
+        "created": "2023-02-14 19:55:03",
+        "shown": "3325",
+        "createdby": "Martin Luther",
+        "active": "1",
+        "comment": "https://github.com/stechstudio/insult-service/",
+        "image": "https://random.imagecdn.app/415/210"
+      }
+]`
