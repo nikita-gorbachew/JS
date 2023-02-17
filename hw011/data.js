27c61aa6d25380349bc24f960fbc11d83f803667
Nikita Gorbachev <nikita_98_gorbachew@mail.ru>
2023-02-17 23:52:59 +0300

    hw11

---
+++ hw011/data.js
@@ -0,0 +1,38 @@
+const data = `[
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+        "price": "$52",
+        "image":"./img/img1.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+        "price": "$52",
+        "image":"./img/img2.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+        "price": "$52",
+        "image":"./img/img3.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+        "price": "$52",
+        "image":"./img/img4.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+        "price": "$52",
+        "image":"./img/img5.png"
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "description":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. ",
+        "price": "$52",
+        "image":"./img/img6.png"
+    }
+]`
